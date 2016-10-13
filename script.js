2334f3207f5f24ea6f978210924aef4a3930a8bf
Steve Larsen <stevelarsen01@gmail.com>
2016-10-13 15:56:40 -0500

    problem 2

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ function nRev() {
 
   document.getElementById("nRev").innerHTML = array.join("");
 }
+
+function f2() {
+  var i2 = document.getElementById('i2').value.split("");
+  var max = (i2.length - 1);
+  var p = true;
+  for (var i = 0; i <= max / 2 && p === true; i++) {
+    p = i2[i] === i2[max - i] ? true : false;
+  }
+  document.getElementById("s2").innerHTML = p === true ? "is" : "is not";
+}
